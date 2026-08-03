a87accffbc38daacc1f30fcb67d47b3ddd691e4a
aarensi <aarensi@it.iliad.com>
2026-08-03 12:20:16 +0200

    fix: add last assets and untrack uncropped assets

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,7 +15,6 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets/js": "assets/js" });
   eleventyConfig.addPassthroughCopy({ "src/assets/fonts": "assets/fonts" });
   eleventyConfig.addPassthroughCopy({ "src/assets/icons": "assets/icons" });
-  eleventyConfig.addPassthroughCopy({ "src/assets/video": "assets/video" });
   eleventyConfig.addPassthroughCopy({ "src/assets/lottie": "assets/lottie" });
   eleventyConfig.addPassthroughCopy({ "src/assets/uploads": "assets/uploads" });
   eleventyConfig.addPassthroughCopy("admin");
